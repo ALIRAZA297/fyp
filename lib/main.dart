0815ec02ebe0e950e2c44341e6c267ca5017a3c7
harram1315 <developerbyharram@gmail.com>
2025-05-02 23:13:15 +0500

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import '../splash_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: "AIzaSyCHQ5NIauBA2zWh2p0_FtbE7xcdUUjvShc",
+      appId: "1:429524941554:android:6f9c600a7b7c9fe1b62e6b",
+      messagingSenderId: "429524941554",
+      projectId: "final-9577b",
+    ),
+  );
   runApp(const MaidConnectApp());
 }
 
